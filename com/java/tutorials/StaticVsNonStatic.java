14462e7cc8c6b197b2b31c24d1d28ae1ecdee0c5
Pankaj <pankaj.kumar@objevsoft.com>
2018-02-24 12:32:27 +0530

    OOPs Basics

---
+++ com/java/tutorials/StaticVsNonStatic.java
@@ -0,0 +1,45 @@
+package com.java.tutorials;
+/*
+* Author Pankaj Kumar (pankajpkm)
+*/
+public class StaticVsNonStatic {
+
+	private static String x= "Static instance Variable";
+	private String y= "Non Static instance Variable";
+	
+	private void instanceMethod(){
+		System.out.println("Inside Instance method in Same Class");
+	}
+	private static void staticMethod(){
+		System.out.println("Inside Static method in Same Class");
+	}
+
+	private static class NestedClass{
+		NestedClass(){
+			System.out.println("Inside NestedClass constructor");
+		}
+		private void show(){
+			System.out.println(x);
+		}
+	}
+	private class InnerClass{
+		InnerClass(){
+			System.out.println("Inside innerClass constructor");
+		}
+		private void show(){
+			System.out.println(x +" "+y);
+		}
+	}
+
+	public static void main(String[] args) {
+		StaticVsNonStatic s=new StaticVsNonStatic();
+		s.instanceMethod();
+		staticMethod();
+		NestedClass nc = new NestedClass();
+		nc.show();
+		StaticVsNonStatic.InnerClass ic = s.new InnerClass();
+		ic.show();
+		
+	}
+
+}
---
+++ com/java/tutorials/TestEquality.java
@@ -0,0 +1,20 @@
+package com.java.tutorials;
+
+public class TestEquality {
+	
+	int x;
+	int y;
+	
+	public boolean testEquality(){
+	
+		return (x==y);
+	}
+	
+	public static void main(String[] args) {
+		TestEquality t = new TestEquality();
+		t.x=5;
+		t.y=5;
+		System.out.println(t.testEquality());
+	}
+
+}
